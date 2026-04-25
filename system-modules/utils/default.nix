eb2acaa97932b52db3c0a169006f325f03290c93
Bence Madarasz <raptor@mbraptor.tech>
2026-04-25 16:08:06 +0200

    finish base system-modules

--- system-modules/utils/default.nix
+++ system-modules/utils/default.nix
@@ -5,15 +5,20 @@
 }:
 {
   environment = {
+    # Avoid putting too much packages here, only essential and system-level stuff should live here!
     systemPackages = with pkgs; [
-      nano
-      git
       curl
-      wget
+      dnsutils
+      ethtool
+      file
+      git
+      gptfdisk
       htop
+      nano
       pciutils
       usbutils
       unzip
+      wget
       zip
     ];
 
