2b2ff37a0ba636ec45abea7e2f7d6e8b5536d20d
Bence Madarasz <raptor@mbraptor.tech>
2026-05-14 12:11:35 +0200

    fix: formatting

--- user-modules/waybar/default.nix
+++ user-modules/waybar/default.nix
@@ -5,7 +5,7 @@
     systemd.enable = true;
 
     settings = builtins.fromJSON (builtins.readFile ./settings.json);
-    
+
     style = builtins.readFile ./style.css; # not using a path literal here, as stylix wants to do stuff
   };
 
