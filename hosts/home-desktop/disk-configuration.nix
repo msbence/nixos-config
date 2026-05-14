e27ce32c3c5071235381a3822982833f469d53ec
Bence Madarasz <raptor@mbraptor.tech>
2026-05-14 06:59:03 +0200

    feat: add disks and FDE everywhere

--- hosts/home-desktop/disk-configuration.nix
+++ hosts/home-desktop/disk-configuration.nix
@@ -11,9 +11,9 @@
       };
     };
     disk = {
-      main = {
+      disk0 = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-WD_Blue_SA510_2.5_1000GB_2227E9460806";
+        device = "/dev/disk/by-id/nvme-CT1000T700SSD3_2411E89EFAFF";
         content = {
           type = "gpt";
           partitions = {
@@ -35,11 +35,11 @@
                 priority = 100;
               };
             };
-            encrypted = {
+            main = {
               size = "100%";
               content = {
                 type = "luks";
-                name = "encrypted";
+                name = "main-encrypted";
                 settings.allowDiscards = true;
                 passwordFile = "/tmp/luks.key";
                 content = {
@@ -52,6 +52,7 @@
                         "subvol=home"
                         "compress=zstd"
                         "relatime"
+                        "ssd"
                       ];
                     };
                     "/nix" = {
@@ -60,6 +61,7 @@
                         "subvol=nix"
                         "compress=zstd"
                         "noatime"
+                        "ssd"
                       ];
                     };
                     "/persisted" = {
@@ -68,9 +70,111 @@
                         "subvol=persisted"
                         "compress=zstd"
                         "relatime"
+                        "ssd"
                       ];
                     };
                   };
+                };
+              };
+            };
+          };
+        };
+      };
+      disk1 = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-KINGSTON_SKC3000S1024G_50026B73827913FF";
+        content = {
+          type = "gpt";
+          partitions = {
+            games-nvme = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "games-nvme-encrypted";
+                settings.allowDiscards = true;
+                passwordFile = "/tmp/luks.key";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  mountpoint = "/games/nvme";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                    "ssd"
+                  ];
+                };
+              };
+            };
+          };
+        };
+      };
+      disk2 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-CT2000MX500SSD1_1918E2008AE3";
+        content = {
+          type = "gpt";
+          partitions = {
+            games-sata = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "games-sata-encrypted";
+                settings.allowDiscards = true;
+                passwordFile = "/tmp/luks.key";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  mountpoint = "/games/sata";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                    "ssd"
+                  ];
+                };
+              };
+            };
+          };
+        };
+      };
+      disk3 = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-Force_MP510_20098263000129531633";
+        content = {
+          type = "mdraid";
+          name = "raid1";
+        };
+      };
+      disk4 = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-Force_MP510_2009826300012953161F";
+        content = {
+          type = "mdraid";
+          name = "raid1";
+        };
+      };
+    };
+    mdadm = {
+      raid1 = {
+        type = "mdadm";
+        level = 1;
+        content = {
+          type = "gpt";
+          partitions = {
+            raid1 = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "raid1-encrypted";
+                settings.allowDiscards = true;
+                passwordFile = "/tmp/luks.key";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/raid1";
+                  mountOptions = [
+                    "defaults"
+                    "relatime"
+                  ];
                 };
               };
             };
--- hosts/home-desktop/settings.nix
+++ hosts/home-desktop/settings.nix
@@ -13,4 +13,11 @@
   userOptions.themeColor = "brown";
 
   home-manager.users.${config.userOptions.username}.programs.git.signing.signByDefault = false;
+
+  systemd.tmpfiles.rules = [
+    # Type Path        Mode UID                            GID    Age Argument
+    "d     /games/nvme 0755 ${config.userOptions.username} users  -   -"
+    "d     /games/sata 0755 ${config.userOptions.username} users  -   -"
+    "d     /raid1      0755 ${config.userOptions.username} users  -   -"
+  ];
 }
